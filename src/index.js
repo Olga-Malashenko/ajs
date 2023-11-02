6cb81baad41de0328a1344b540f0cc3549eb80d5
Olga Malashenko <olga_birjukova@mail.ru>
2023-11-02 17:10:33 +0300

    Package done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './css/style.css';
 import './js/app.js';
+
+function info() {
+    const m = new Map();
+    m.set('name', 'ajs');
+    m.set('version', '1.0.0');
+    return m;
+  }
+  
+  export default {
+    info
+  };
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'), // каталог для результатов сборки 
+        filename: 'index.js', // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
+        library: 'ajs', // название нашей библиотеки
+        libraryTarget: 'umd', // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
+        libraryExport: 'default', // экспортируется имя default
+        globalObject: 'this', // что принимать за глобальный объект, иначе сгенерируется window, а его, как вы знаете, на платформе Node.js нет
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /\node_modules/,
-                loader: 'babel-loader'
+                use: {
+                    loader: 'babel-loader'
+                }
             },
             {
                 test: /\.css$/,
